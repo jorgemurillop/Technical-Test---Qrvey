c8a993b7c50765b100f0a0bc5761eb4bf4ea7f1f
jorgemurillop <64759826+jorgemurillop@users.noreply.github.com>
2020-05-05 16:57:02 -0500

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,13 +13,12 @@ api.get('/project', projectControllers.get);
 api.get('/project/alls', auth, projectControllers.getAlls);
 api.get('/project/myTime', auth, projectControllers.getMyTime);
 api.get('/project/usersTime', auth, projectControllers.getUsersTime);
-
 //AllowAnonymous
-api.get('/project/:projectId', projectControllers.getById);
+api.get('/project/getById', projectControllers.getById);
 //Authorize
 api.post('/project', auth, projectControllers.save);
-api.put('/project/:projectId', auth, projectControllers.update);
-api.delete('/project/:projectId', auth, projectControllers.remove);
+api.put('/project', auth, projectControllers.update);
+api.delete('/project', auth, projectControllers.remove);
 
 
 //Authorize
@@ -28,15 +27,14 @@ api.get('/user', auth, userControllers.get);
 api.post('/signUp', userControllers.signUp);
 api.post('/signin', userControllers.signIn);
 
+
 //Authorize
 api.get('/task', auth, taskControllers.get);
-api.get('/task/:taskId', auth, taskControllers.getById);
-api.put('/task/:taskId', auth, taskControllers.update);
+api.get('/task/getById', auth, taskControllers.getById);
+api.get('/task/getByProjectId', auth, taskControllers.getByProjectId);
+api.put('/task', auth, taskControllers.update);
 api.post('/task', auth, taskControllers.save);
-api.delete('/task/:taskId', auth, taskControllers.remove);
-
-
-//Authorize
+api.delete('/task', auth, taskControllers.remove);
 api.post('/task/start', auth, taskControllers.start);
 api.post('/task/stop', auth, taskControllers.stop);
 api.post('/task/pause', auth, taskControllers.pause);
